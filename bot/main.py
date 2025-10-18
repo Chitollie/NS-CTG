ef5270f38c14101dca4978efaa237eb8a2da8d0e
Chitollie <chitollie@pm.me>
2025-10-18 01:05:47 +0000

    update

--- bot/main.py
+++ bot/main.py
@@ -3,9 +3,7 @@ from discord.ext import commands
 from bot.config import TOKEN, GUILD_ID
 from bot.events import setup_events
 from bot.commands.menu import menu_cmd
-from bot.web_server import keep_alive
 from bot.commands.annonces import annonces_cmd
-import asyncio
 
 intents = discord.Intents.default()
 intents.members = True
@@ -14,14 +12,11 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def setup_hook():
+    # register the menu command for the guild
     bot.tree.add_command(menu_cmd, guild=discord.Object(id=GUILD_ID))
     bot.tree.add_command(annonces_cmd, guild=discord.Object(id=GUILD_ID))
     await setup_events(bot)
 
-
-keep_alive()
-
-asyncio.run(setup())
 
 if TOKEN is None:
     raise RuntimeError("TOKEN n'est pas défini dans le fichier .env")
