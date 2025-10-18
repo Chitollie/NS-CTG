f7c2903e73c8cef9ae80044760b0923a5cd9dce3
Chitollie <chitollie@pm.me>
2025-10-18 01:44:11 +0000

    update

--- bot/embeds/tarifs.py
+++ bot/embeds/tarifs.py
@@ -1,14 +1,23 @@
 import discord
+from ..config import TARIF_CHANNEL_ID
 
-def tarifs_embed():
+
+async def send_tarifs(client : discord.Client):
     embed = discord.Embed(
         title="💰 Tarifs - Nova Sécurité",
-        description="Voici un aperçu des tarifs de base. Utilisez la simulation pour obtenir une estimation précise.",
-        color=discord.Color.gold()
-    )
-    embed.add_field(name="Frais de base", value="50 000 $ (coûts de services)", inline=False)
+        description=
+        "Voici un aperçu des tarifs de base. Utilisez la simulation pour obtenir une estimation précise.",
+        color=discord.Color.gold())
+    embed.add_field(name="Frais de base",
+                    value="50 000 $ (coûts de services)",
+                    inline=False)
     embed.add_field(name="Recrue", value="10 000 $ / agent", inline=True)
-    embed.add_field(name="Agent confirmé", value="12 500 $ / agent", inline=True)
+    embed.add_field(name="Agent confirmé",
+                    value="12 500 $ / agent",
+                    inline=True)
     embed.add_field(name="Responsable", value="15 000 $ / agent", inline=True)
     embed.set_footer(text="Simulation disponible via le /menu")
-    return embed
+
+    channel =  client.get_channel(TARIF_CHANNEL_ID)
+    if channel:
+        await channel.send(embed=embed)
