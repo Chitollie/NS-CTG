c96faf512d628db3eaf1d29bd00160d8f7791c7d
Chitollie <chitollie@pm.me>
2025-10-31 15:04:12 +0100

    update

--- bot/main.py
+++ bot/main.py
@@ -9,6 +9,14 @@ from bot.utils.join import setup_join
 from bot.embeds import contacts
 from bot.views import identification_view, askmiss_view
 from bot.views.mission_admin_view import feedback_states, send_note_request, send_comment_request, send_recap, send_modify_choice
+from bot.embeds import tarifs, localisation
+from bot import config
+
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Handler déplacé ici après la définition de bot
 @bot.event
 async def on_message(message):
     if message.author.bot:
