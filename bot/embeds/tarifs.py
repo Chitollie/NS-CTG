fa28d25cd2deb139ffd60e9fca3c7bd72ca6fd7e
Chitollie <chitollie@pm.me>
2025-12-16 16:46:08 +0100

    update

--- bot/embeds/tarifs.py
+++ bot/embeds/tarifs.py
@@ -28,13 +28,7 @@ async def send_tarifs(client: discord.Client):
     #embed.add_field(name="Frais de base",
                     #value="20 000 $ (coûts de services)",
                     #inline=False)
-    embed.add_field(name="Recrue", value="10 000 $ / 30 minutes", inline=True)
-    embed.add_field(name="Agent confirmé",
-                    value="12 500 $ / 30 minutes",
-                    inline=True)
-    embed.add_field(name="Responsable",
-                    value="15 000 $ / 30 minutes",
-                    inline=True)
+    embed.add_field(name="Agents", value="10 000 $ / 30 minutes", inline=True)
     embed.set_footer(text="Simulation disponible via le /menu")
 
     await clean_and_send(channel, embed=embed, bot_filter=MESSAGE_IDENTIFIER)
